b7ad7e11477b238eb2fdb89e203098d0d1c1d6e7
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-09-18 17:48:50 +0200

    course, faculty record

---
+++ Backend01B/Faculty.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend01B
+{
+    public record Course(string name, int credit);
+
+    public record Faculty(string name, Course[] courses);
+}
