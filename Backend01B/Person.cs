9ee95f78b3d1a226e6381de328930f3c8b70c62e
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-09-18 16:14:19 +0200

    person class, elements

---
+++ Backend01B/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend01B
+{
+    //a class for a person with name, age, job, haircolor, height
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Job { get; set; }
+        public string HairColor { get; set; }
+        public double Height { get; set; }
+
+        public Person()
+        {
+            
+        }
+
+        public Person(string name, int age, string job, string hairColor, double height)
+        {
+            Name = name;
+            Age = age;
+            Job = job;
+            HairColor = hairColor;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Job: {Job}, HairColor: {HairColor}, Height: {Height}";
+        }
+    }
+}
--- Backend01B/Program.cs
+++ Backend01B/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            //random komment
+            //a person list with 10 elements
+            List<Person> persons = new List<Person>
+            {
+                new Person("John", 25, "Developer", "Black", 1.75),
+                new Person("Jane", 30, "Manager", "Blonde", 1.65),
+                new Person("Jack", 35, "CEO", "Brown", 1.85),
+                new Person("Jill", 40, "CTO", "Red", 1.70),
+                new Person("James", 45, "CFO", "Black", 1.80),
+                new Person("Jenny", 50, "COO", "Blonde", 1.60),
+                new Person("Joe", 55, "CIO", "Brown", 1.90),
+                new Person("Jade", 60, "CISO", "Red", 1.55),
+                new Person("Josh", 65, "CPO", "Black", 1.95),
+                new Person("Jasmine", 70, "CLO", "Blonde", 1.50)
+            };
         }
     }
 }
