c615ab4868d18942a87e7754d30dadc14175d78c
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-09-18 16:03:09 +0200

    Add project files.

---
+++ Backend01B/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend01B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
